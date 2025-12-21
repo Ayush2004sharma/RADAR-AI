83dd1fd0d7ff01bdfa4df345d0d19f1fe840cfac
Ayush2004sharma <ayush.sharma.cseds.2022@miet.iac.in>
2025-12-21 11:21:56 +0530

    commit

--- stream-processor/consumer.py
+++ stream-processor/consumer.py
@@ -3,29 +3,33 @@ from aggregator import increment_error
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
-mongo = MongoClient(os.getenv("MONGO_URI"))
-db = mongo[os.getenv("MONGO_DB")]
-logs_collection = db[os.getenv("MONGO_COLLECTION")]
+def start_consumer():
+    mongo = MongoClient(os.getenv("MONGO_URI"))
+    db = mongo[os.getenv("MONGO_DB")]
+    logs_collection = db[os.getenv("MONGO_COLLECTION")]
 
-last_id = "0-0"
+    last_id = "0-0"
 
-print("Consumer started...")
+    print("Consumer started...")
 
-while True:
-    streams = read_logs(last_id)
+    while True:
+        streams = read_logs(last_id)
 
-    for stream, messages in streams:
-        for msg_id, log in messages:
-            print("Consumed:", log)
+        for stream, messages in streams:
+            for msg_id, log in messages:
+                print("Consumed:", log)
 
-            # Store log in MongoDB
-            logs_collection.insert_one(log)
+                # Store log in MongoDB
+                logs_collection.insert_one(log)
 
-            # Aggregate errors
-            if log.get("level") == "ERROR":
-                increment_error(log.get("service"))
+                # Aggregate errors
+                if log.get("level") == "ERROR":
+                    increment_error(log.get("service"))
 
-            last_id = msg_id
+                last_id = msg_id
+
+        time.sleep(0.1)  # 🔥 prevents CPU overuse
